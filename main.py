cd3c6706d0e5b6fc07831ef9ac3ee17b58aeb7c0
@yandexliceq <kamilovmehrubon@gmail.com>
2023-11-29 17:59:58 +0300

    bank class

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-def test(test):
-    print(test)
+class Bank:
+    def __init__(self):
+        self.clients = []
+    
+    def add_client(self, client):
+        self.clients.append(client)
+    def remove_client(self, client):
+        self.clients = list(filter(clients, lambda x: x.id != client.id))
+    def all_cash(self):
+        res = 0 
+        for client i self.clients:
+            res += client.money
+        
+        return res
